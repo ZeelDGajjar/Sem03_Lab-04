d444e2a6ce225ff8285b093e256be2b7093b47a1
2474008 <2474008@F40003-CB42G24.VanierCollege.intra>
2025-09-22 15:17:05 -0400

    Built basic structure since everything was erased

--- src/sem3_lab04/Sem3_Lab04.java
+++ src/sem3_lab04/Sem3_Lab04.java
@@ -8,13 +8,21 @@ package sem3_lab04;
  *
  * @author 2474008
  */
-public class Sem3_Lab04 implements Applications {
+public class Sem3_Lab04 implements Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
-    
+
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("Business App"); 
+        Label label = new Label("Press a key"); 
+
+        stage.setScene(scene); 
+        stage.show(); 
+    }
 }
