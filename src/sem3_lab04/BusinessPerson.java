7219576da9de7e7d1ea372d63c031fb185a3f890
Z.G. <zeel.gajjar2007@gmail.com>
2025-09-22 20:59:35 -0400

    Implemented almost methods in BusinessPerson.java

--- src/sem3_lab04/BusinessPerson.java
+++ src/sem3_lab04/BusinessPerson.java
@@ -16,6 +16,78 @@ public class BusinessPerson {
     double amtOfParkingFees;
     double amtOfTaxiCharges;
     double conferenceRegistrationFees;
-    
-    
+    double LodgingFees;
+
+    public BusinessPerson(int numOfDays) {
+        this.numOfDays = numOfDays;
+    }
+
+    public BusinessPerson(int numOfDays, double amtOfAirfare, double amtOfCarRentalFees, double numOfMiles, double amtOfParkingFees, double amtOfTaxiCharges, double conferenceRegistrationFees, double LodgingFees) {
+        this.numOfDays = numOfDays;
+        this.amtOfAirfare = amtOfAirfare;
+        this.amtOfCarRentalFees = amtOfCarRentalFees;
+        this.numOfMiles = numOfMiles;
+        this.amtOfParkingFees = amtOfParkingFees;
+        this.amtOfTaxiCharges = amtOfTaxiCharges;
+        this.conferenceRegistrationFees = conferenceRegistrationFees;
+        this.LodgingFees = LodgingFees;
+    }
+
+    public double calculateMeals() {
+        return numOfDays * 37;
+    }
+
+    public double calculateParkingFees() {
+        return numOfDays * 10.00;
+    }
+
+    public double calculateTaxiCharges() {
+        return numOfDays * 20.00;
+    }
+
+    public double calculateLodgingCharges() {
+        return numOfDays * 95.00;
+    }
+
+    public double calculateExtraParkingFees() {
+        if (amtOfTaxiCharges != 0) {
+            return numOfMiles * 0.27;
+        }
+        return 0;
+    }
+
+    public double calculateTotalExpenses() {
+        return amtOfAirfare + amtOfCarRentalFees + amtOfParkingFees + amtOfTaxiCharges + conferenceRegistrationFees + LodgingFees;
+    }
+
+    public double calculateTotalAllowableCharges() {
+        return calculateMeals() + calculateParkingFees() + calculateTaxiCharges() + calculateLodgingCharges() + calculateExtraParkingFees();
+    }
+
+    public double calculateExcessToPay() {
+        if (calculateTotalExpenses() > calculateTotalAllowableCharges()) {
+            return calculateTotalExpenses() - calculateTotalAllowableCharges();
+        }
+        return 0;
+    }
+
+    public double calculateAmountSaved() {
+        if (calculateTotalAllowableCharges() > calculateTotalExpenses()) {
+            return calculateTotalAllowableCharges() - calculateTotalExpenses();
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BusinessPerson{" +
+                "numOfDays=" + numOfDays +
+                ", amtOfAirfare=" + amtOfAirfare +
+                ", amtOfCarRentalFees=" + amtOfCarRentalFees +
+                ", numOfMiles=" + numOfMiles +
+                ", amtOfParkingFees=" + amtOfParkingFees +
+                ", amtOfTaxiCharges=" + amtOfTaxiCharges +
+                ", conferenceRegistrationFees=" + conferenceRegistrationFees +
+                '}';
+    }
 }
